d765cc591269ac6700dc0a319e3cd80eb3347ead
David Sáenz <dsaenz@bebanjo.com>
2021-07-08 16:00:53 +0200

    Webpack: change entry point

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,2 +1,1 @@
 import * as Turbo from "@hotwired/turbo"
-
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './public/index.js',
+  entry: './public/javascript/index.js',
   output: {
     path: __dirname + '/public/dist',
     filename: 'index.bundle.js'
